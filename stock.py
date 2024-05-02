d9c0c5f45fa430acbd0de489adb477dc5e101e78
Bonnaire Fils Emmanuel <149618934+illusionistr@users.noreply.github.com>
2024-05-02 18:57:14 -0400

    cumret deletion

--- stock.py
+++ stock.py
@@ -11,14 +11,10 @@ dropdown = sl.multiselect('Choose your  ticker', tickers)
 start = sl.date_input('Start date', pd.to_datetime('2019-01-01'))
 end = sl.date_input('End date', pd.to_datetime('today'))
 
-def relret(df):
-    rel = df.pct_change()
-    cumret = (1+rel).cumprod() -1
-    cumret = cumret.fillna(0)
-    return cumret
+
 
 if len(dropdown) > 0:
-    df = relret(yf.download(dropdown, start, end)['Adj Close'])
+    df = (yf.download(dropdown, start, end)['Adj Close'])
     sl.line_chart(df)
 
 
