f05a35274835438b986651dfb9ce038433820e36
Bonnaire Fils Emmanuel <149618934+illusionistr@users.noreply.github.com>
2024-05-02 18:01:45 -0400

    dropdown add

--- stock.py
+++ stock.py
@@ -6,6 +6,10 @@ import difflib
 start = sl.date_input('Start date', pd.to_datetime('2019-01-01'))
 end = sl.date_input('End date', pd.to_datetime('today'))
 
+def get_suggestions(search_input):
+                suggestions = difflib.get_close_matches(search_input, ticker_dict.keys())
+                return suggestions
+
 def get_ticker_dict():
         df = pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')[0]
         ticker_dict = dict(zip(df['Security'], df['Symbol']))
@@ -13,19 +17,32 @@ def get_ticker_dict():
 
 ticker_dict = get_ticker_dict()
 
-search_input = sl.text_input('Search by company name or symbol')
-if search_input:
-    if search_input in ticker_dict.values():
-        # Search by symbol
-        symbol = search_input
-    elif search_input in ticker_dict.keys():
-        # Search by company name
-        symbol = ticker_dict[search_input]
-    else:
-        sl.write(f"No search result found for '{search_input}'")
-        symbol = None
+search_input = sl.text_input('Search by company name or symbol', key='search_input')
 
-    if symbol:
+# Create a list of suggestions based on the search input
+suggestions = get_suggestions(search_input)
+
+# Display a dropdown of suggestions while typing
+selected_suggestion = sl.selectbox('Suggestions', suggestions)
+
+if selected_suggestion:
+    symbol = ticker_dict[selected_suggestion]
+    data = yf.download(symbol, start, end)
+    if not data.empty:
+        sl.write(f"Search result for '{selected_suggestion}':")
+        sl.line_chart(data['Adj Close'])
+        # Display graph for GSPC
+        gspc_data = yf.download('^GSPC', start, end)
+        if not gspc_data.empty:
+            sl.write("S&P 500 (^GSPC):")
+            sl.line_chart(gspc_data['Adj Close'])
+        else:
+            sl.write("No data found for GSPC")
+    else:
+        sl.write(f"No data found for '{selected_suggestion}'")
+
+
+if symbol:
         data = yf.download(symbol, start, end)
         if not data.empty:
             sl.write(f"Search result for '{search_input}':")
@@ -40,9 +57,7 @@ if search_input:
         else:
             sl.write(f"No data found for '{search_input}'")
 
-def get_suggestions(search_input):
-                suggestions = difflib.get_close_matches(search_input, ticker_dict.keys())
-                return suggestions
+
 
 suggestions = get_suggestions(search_input)
 
